7de37bae62732df55c4653a5434f9ed0adb1bfe9
ujin_bang <qkddnwls14@gmail.com>
2022-02-21 10:11:27 +0900

    탭 레이아웃 배치/ 뷰페이저 연결

--- app/src/main/java/com/example/viewpager_fragment_20220218/MainActivity.kt
+++ app/src/main/java/com/example/viewpager_fragment_20220218/MainActivity.kt
@@ -27,5 +27,9 @@ class MainActivity : AppCompatActivity() {
 //        재사용성 무시하도록 하는 코드예시(사용성 개선)
         mainViewPager.offscreenPageLimit = 5
 
+//        탭 레이아웃 / 뷰페이저 연결
+
+        mainTabLayout.setupWithViewPager(mainViewPager)
+
     }
 }
